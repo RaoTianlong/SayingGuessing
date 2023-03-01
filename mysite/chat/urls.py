6c16b17d4b6252c7d088f6ae49417f402d100f1a
raotl <raotl@jiguang.cn>
2023-03-01 18:57:20 +0800

    使用admin的用户管理控制登陆

--- mysite/chat/urls.py
+++ mysite/chat/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
+    path('register/', views.register, name='register'),
+    path("<str:room_name>/", views.room, name="room"), # 按顺序匹配，通配符得最后
 ]
--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def register(request):
+    print(request)
+    return HttpResponse('not support to register')
+
+
+@login_required
 def index(request):
+    print(request.user)
     return render(request, "chat/index.html")
 
 
+@login_required
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
 
